a72a52e08e9550c8d20fae0b5a15ddce0c9e4a6a
Ahmed Dabbous <ahmeddabbous67@gmail.com>
2023-10-22 19:10:56 +0200

    Semi-functional. Tiling is glitchy. Come back here if failed.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 _side = 20
-x_steps = screen.get_width() // _side
+x_steps = screen.get_width() // (2 * _side)
 y_steps = math.ceil(screen.get_height() / (_side * np.sin(np.pi / 3))) + 1
 
 
@@ -29,8 +29,6 @@ def gen_hexagon(center: pygame.Vector2 = None, side: int = None):
 def gen_grid(side: int = None):
     if side is None:
         side = 20
-    print(x_steps)
-    print(y_steps)
     # start from top right corner of screen
     # move right and down
     for row in range(0, y_steps):
@@ -54,17 +52,78 @@ def gen_grid(side: int = None):
 
 screen.fill("purple")
 # gen_grid()
-gen_grid(_side)
-while running:
-    # this array should contain the (row, col) positions of the snake
-    snake_arr = [(31, 20), (33, 20), (35, 20)]
+blob_collected = True
+_rand_row = np.random.randint(0,y_steps)
+_rand_col = np.random.randint(0,x_steps)
+snake_arr = [(5, 1), (6, 1), (7, 1)]
 
-    blob_collected = True
+up_dir = True
+right_dir = None
+
+while running:
+    gen_grid(_side)
+    # this array should contain the (row, col) positions of the snake
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_a]:
+        right_dir = False
+    elif keys[pygame.K_d]:
+        right_dir = True
+    elif keys[pygame.K_w]:
+        up_dir = True
+        right_dir = None
+    elif keys[pygame.K_s]:
+        up_dir = False
+        right_dir = None
+
+    if up_dir and right_dir is None:
+        _new_row = snake_arr[0][0] - 2
+        _new_col = snake_arr[0][1]
+        snake_arr.insert(0, (_new_row, _new_col))
+        snake_arr.pop()
+    elif not up_dir and right_dir is None:
+        _new_row = snake_arr[0][0] + 2
+        _new_col = snake_arr[0][1]
+        snake_arr.insert(0, (_new_row, _new_col))
+        snake_arr.pop()
+    elif up_dir and right_dir:
+        _new_row = snake_arr[0][0] - 1
+        _new_col = snake_arr[0][1] + 1
+        snake_arr.insert(0, (_new_row, _new_col))
+        snake_arr.pop()
+    elif not up_dir and right_dir:
+        _new_row = snake_arr[0][0] + 1
+        _new_col = snake_arr[0][1] + 1
+        snake_arr.insert(0, (_new_row, _new_col))
+        snake_arr.pop()
+    elif up_dir and not right_dir:
+        _new_row = snake_arr[0][0] - 1
+        _new_col = snake_arr[0][1] - 0
+        snake_arr.insert(0, (_new_row, _new_col))
+        snake_arr.pop()
+    elif not up_dir and not right_dir:
+        _new_row = snake_arr[0][0] + 1
+        _new_col = snake_arr[0][1] - 0
+        snake_arr.insert(0, (_new_row, _new_col))
+        snake_arr.pop()
+
+    for pt in snake_arr:
+        print(snake_arr)
+        if pt[0] % 2 == 0:
+            _offset = 0
+        else:
+            _offset = 1.5
+
+        center = _side * pygame.Vector2(3 * pt[1] + _offset, pt[0] * np.sin(np.pi / 3))
+        pt_hex = gen_hexagon(
+            center=center,
+            side=0.8 * _side
+        )
+        pygame.draw.polygon(surface=screen, color="blue", points=pt_hex, )
+
     if blob_collected:
         # randomly select blob point
-        _rand_row = np.random.randint(0,y_steps)
-        _rand_col = np.random.randint(0,x_steps)
-
         _color = pygame.Color(255 * np.random.random(size=(3,)))
 
         # draw the random blob
@@ -78,7 +137,8 @@ while running:
             center=center,
             side=0.8*_side
         )
-        pygame.draw.polygon(surface=screen, color=_color, points=blob_hex, )
+        # pygame.draw.polygon(surface=screen, color=_color, points=blob_hex, )
+        pygame.draw.polygon(surface=screen, color="red", points=blob_hex, )
         blob_collected = False
 
     # poll for events
@@ -87,9 +147,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # fill the screen with a color to wipe away anything from last frame
-
-
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -97,7 +154,7 @@ while running:
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(20) / 1000
 
 
 pygame.quit()
